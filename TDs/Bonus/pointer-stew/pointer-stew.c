3635523806b6731dd3c64df43149099db71d102a
Florian Latapie <florian.latapie@etu.unice.fr>
2022-10-28 09:20:51 +0200

    pointer-stew

---
+++ TDs/Bonus/pointer-stew/pointer-stew.c
@@ -0,0 +1,12 @@
+char *c[] = {"ENTER", "NEW", "POINT", "FIRST"};
+char **cp[] = { c+3, c+2, c+1, c };
+char ***cpp = cp;
+
+int main(void)
+{
+	printf("%s",  **++cpp);
+	printf("%s ", *--*++cpp+3);
+	printf("%s",  *cpp[-2]+3);
+	printf("%s",  cpp[-1][-1]+1);
+	return 0;
+}
